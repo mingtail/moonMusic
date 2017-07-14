061e24ae2da075ba222e537bac616a3fd7ee8a10
mingtail <mingtail@qq.com>
2017-07-14 17:15:37 +0800

    moonMusic
    my music player

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    推荐页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style type="text/css">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+// Vue.config.productionTip = false  //关闭生产模式下给出的提示.
+
+import 'common/stylus/index.styl'
+
+// fastclick固定用法, 绑定document.body, body下的点击没有延迟
+fastclick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
