ae25f034faad18e7d32c4a02adc3c2cc16d93acb
mingtail <mingtail@qq.com>
2017-11-27 15:44:17 +0800

    add new

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import router from './router'
 import fastclick from 'fastclick'
 // Vue.config.productionTip = false  //关闭生产模式下给出的提示.
 
-// 公共样式可以在main.js里面引入
 import 'common/stylus/index.styl'
 
-// fastclick固定用法, 绑定document.body, body下的点击没有延迟
 fastclick.attach(document.body)
 
 /* eslint-disable no-new */
