0099c85d8f6a86a4d09688f195bddc0c36d13b4f
mingtail <mingtail@qq.com>
2017-11-28 17:41:01 +0800

    add new

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,19 +1,45 @@
 <template>
-  <div class="recommend">
+  <div class="recommend" ref="recommend">
     <div class="recommend-content">
       <div class="slider-wrapper">
 
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
-        <ul></ul>
+        <ul>
+          <li v-for="item in discList" class="item">
+
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import { getRecommend } from 'api/recommend'
+  import { ERR_OK } from 'api/config'
 
+  export default {
+    data() {
+      return {
+        recommends: [],
+        discList: []
+      }
+    },
+    created() {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.slider)
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
